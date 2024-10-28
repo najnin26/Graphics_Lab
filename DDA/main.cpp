35a90c25f2e4509a529d640d7ed2962da66d7d8d
najnin26 <najninshirin26@gmail.com>
2024-10-28 15:37:55 +0600

    dda line drawing algorithm

---
+++ DDA/main.cpp
@@ -0,0 +1,93 @@
+#include <GL/glut.h>
+#include <stdlib.h>
+#include<stdio.h>
+float x1,x2,y1,y2,m;
+float dx,dy;
+
+void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glEnd();
+
+    glColor3f(0.0,1.0,0.0);
+
+    glBegin(GL_POINTS);
+
+    if(m>0 && m<=1)
+    {
+        while(x1<=x2 && y1<=y2)
+        {
+            x1=x1+1;
+            y1=y1+m;
+            glVertex3f(x1/100,y1/100,0.0);
+            printf("%f %f",x1,y1);
+        }
+    }
+    else if(m>1)
+    {
+        while(x1<=x2 && y1<=y2)
+        {
+            x1=x1+(1/m);
+            y1=y1+1;
+            glVertex3f(x1/100,y1/100,0.0);
+            printf("%f %f",x1,y1);
+        }
+    }
+    else if(m>-1 && m<=0)
+    {
+        while(x1>=x2 && y1>=y2)
+        {
+            x1=x1-1;
+            y1=y1-m;
+            glVertex3f(x1/100,y1/100,0.0);
+            printf("%f %f",x1,y1);
+        }
+    }
+    else if(m<-1)
+    {
+        while(x1>=x2 && y1>=y2)
+        {
+            x1=x1-(1/m);
+            y1=y1-1;
+            glVertex3f(x1/100,y1/100,0.0);
+            printf("%f %f",x1,y1);
+        }
+    }
+    glEnd();
+    glFlush();
+}
+
+void init(void)
+{
+    glClearColor(0.0,0.0,0.0,0.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0,1.0,0.0,1.0,-1.0,1.0);
+}
+
+int main(int argc, char *argv[])
+{
+    printf("Enter value of X1 :");
+    scanf("%f",&x1);
+    printf("Enter value of y1 :");
+    scanf("%f",&y1);
+    printf("Enter value of X2 :");
+    scanf("%f",&x2);
+    printf("Enter value of Y2 :");
+    scanf("%f",&y2);
+    dx=x2-x1;
+    dy=y2-y1;
+    m=dy/dx;
+
+    glutInit(&argc, argv);
+    glutInitWindowSize(500,500);
+    glutInitWindowPosition(100,100);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("DDA Line");
+    init();
+    glutDisplayFunc(display);
+
+    glutMainLoop();
+
+    return 0;
+}
