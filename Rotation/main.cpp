6d33336777640b75ef09a67ec1f9bd40cf7e7a8b
najnin26 <najninshirin26@gmail.com>
2024-10-29 13:09:00 +0600

    Update main.cpp

--- Rotation/main.cpp
+++ Rotation/main.cpp
@@ -48,56 +48,44 @@ void display()
     glPushMatrix();
     glTranslatef(0.0f,-1.0f,0.0f);
     glRotatef(_angle,0.0f,0.0f,-1.0f);
-
     glColor3f(1.0,1.0,1.0);
-
     //Rectangle
     glBegin(GL_QUADS);
-
     glColor3f(1.0,0.0,0.0);
-
 	glColor3f(1.0f, 0.0f, 0.0f);
 	glVertex3f(-0.7f, -0.5f, 0.0f);
 	glVertex3f(0.7f, -0.5f, 0.0f);
 	glVertex3f(0.7f, 0.5f, 0.0f);
 	glVertex3f(-0.7f, 0.5f, 0.0f);
-
     glEnd();
-
     glPopMatrix();
+
     glPushMatrix();
     glTranslatef(1.0f,1.0f,0.0f);
     glRotatef(_angle,0.0f,1.0f,0.0f);
-
-
     // Pentagon
     glBegin(GL_POLYGON);
-
     glColor3f(0.0,1.0,0.0);
-
     glVertex3f(-0.5f, -0.5f, 0.0f);
     glVertex3f(0.5f, -0.5f, 0.0f);
     glVertex3f(0.5f, 0.0f, 0.0f);
     glVertex3f(0.0f, 0.50f, 0.0f);
     glVertex3f(-0.5f, 0.0f, 0.0f);
     glEnd();
-
     glPopMatrix();
+
     glPushMatrix();
     glTranslatef(-1.0f,1.0f,0.0f);
     glRotatef(_angle,1.0f,0.0f,0.0f);
-
     // Triangle
     glBegin(GL_TRIANGLES);
-
     glColor3f(0.0,0.0,1.0);
-
     glVertex3f(-0.50f, -0.5f, 0.0f);
     glVertex3f(0.50f, -0.5f, 0.0f);
     glVertex3f(0.0f, 0.5f, 0.0f);
     glEnd();
-
     glPopMatrix();
+
     glutSwapBuffers();
 }
 
